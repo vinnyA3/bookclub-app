15b561efd450866184a5acdb0f3a77ef68768ac9
vinnya3 <vinaceto@yahoo.com>
2016-02-25 15:51:59 -0500

    inital commit

---
+++ server.js
@@ -0,0 +1,53 @@
+var express = require('express'),
+    app = express(),
+    server = require('http').Server(app),
+    bodyParser = require('body-parser'),
+    path = require('path'),
+    config = require('./config/config'),
+    morgan = require('morgan'),
+    mongoose = require('mongoose'),
+    sass = require('node-sass-middleware'),
+    port = process.env.PORT || 8080;
+
+//connect to mongoose database
+mongoose.connect(config.db);
+
+//use morgan to log all requests
+app.use(morgan('dev'));
+
+//body parser middleware
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+//cors config
+app.use(function(req,res,next){
+  res.setHeader('Access-Control-Allow-Origin','*');
+  res.setHeader('Access-Control-Allow-Methods','GET,POST,DELETE,PUT');
+  res.setHeader('Access-Control-Allow-Headers','X-Requested-With, content-type, \ Authorization');
+  next();
+});
+
+//sass middleware
+app.use(
+  sass({
+    src: __dirname + '/public/sass',
+    dest: __dirname + '/public/assets/styles',
+    force: true,
+    debug: true,
+    outputStyle: 'compressed'
+  })
+);
+
+//set the location for the static files
+app.use(express.static(path.join(__dirname,'/public')));
+
+//get * route
+app.get('*', function(req,res){
+  res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
+
+//start server and listen on port
+server.listen(port, function(){
+  console.log('Server is listening on port: ' + port + '...');
+});
+
