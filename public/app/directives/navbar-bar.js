c2363bb2dc40bc5da6a898aeefe02654da4f3ff7
vinnya3 <vinaceto@yahoo.com>
2016-02-26 00:40:24 -0500

    added hamburger button directive

---
+++ public/app/directives/navbar-bar.js
@@ -0,0 +1,26 @@
+angular.module('navbarButtonDirective',[])
+  .directive('navbarToggle',function(){
+    return {
+      restrict:'E',
+      template:'<div><span class="main-hamburger fa fa-bars"></span><div>',
+      replace:true,
+      link:function(scope,element,attrs){
+        var mainView = element.parent().parent().parent(),
+            mainDiv = element.parent().parent(),
+            nav = mainView[0].querySelector('.drawer-nav');
+            console.log(mainView);
+            console.log(mainDiv);
+            console.log(nav);
+        element.on('click',function(e){
+          nav.classList.toggle('js-drawer-nav-open');
+          e.stopPropagation();
+        });
+        mainDiv.on('click', function(){
+          nav.classList.remove('js-drawer-nav-open');
+        })
+      },
+      controller:function($scope){
+        console.log('operational');
+      }
+    }
+  });
