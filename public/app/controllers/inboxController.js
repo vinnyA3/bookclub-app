af4fb0b69a7b3ab4d535b87d4078998254b9f79a
vinnya3 <vinaceto@yahoo.com>
2016-02-26 17:39:18 -0500

    new files

---
+++ public/app/controllers/inboxController.js
@@ -0,0 +1,15 @@
+angular.module('inboxCtrl', [])
+  .controller('inboxController', function(){
+    var vm = this;
+    vm.swapRequests = [
+      {name: 'Julie', book: 'Cosmos'},
+      {name: 'Julie', book: 'Cosmos'},
+      {name: 'Julie', book: 'Cosmos'},
+      {name: 'Julie', book: 'Cosmos'},
+      {name: 'Julie', book: 'Cosmos'},
+      {name: 'Julie', book: 'Cosmos'},
+      {name: 'Julie', book: 'Cosmos'},
+      {name: 'Julie', book: 'Cosmos'},
+      {name: 'Julie', book: 'Cosmos'},
+    ]
+  });
