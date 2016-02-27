c08033cd7f469f75bc3c400ce0c61ff4e18ef59b
vinnya3 <vinaceto@yahoo.com>
2016-02-26 21:43:32 -0500

    added approved object

--- public/app/controllers/inboxController.js
+++ public/app/controllers/inboxController.js
@@ -1,6 +1,11 @@
 angular.module('inboxCtrl', [])
   .controller('inboxController', function(){
+
     var vm = this;
+
+    vm.modalShown = false;
+    vm.approvedInfo = {};
+
     vm.swapRequests = [
       {name: 'Julie', book: 'Cosmos'},
       {name: 'Julie', book: 'Cosmos'},
@@ -11,5 +16,12 @@ angular.module('inboxCtrl', [])
       {name: 'Julie', book: 'Cosmos'},
       {name: 'Julie', book: 'Cosmos'},
       {name: 'Julie', book: 'Cosmos'},
-    ]
+    ];
+
+    vm.toggleModal = function(name,book){
+        vm.modalShown = !vm.modalShown;
+        vm.approvedInfo.name = name;
+        vm.approvedInfo.book = book;
+    };
+
   });
