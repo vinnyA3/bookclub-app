7e73644767d5abdea5f736d3f313d494b5f936d5
vinnya3 <vinaceto@yahoo.com>
2016-02-29 00:47:10 -0500

    added temp secret to config

--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
 module.exports = {
-  'db': 'mongodb://127.0.0.1:27017/bookclub_app' //remote
+  'db': 'mongodb://127.0.0.1:27017/bookclub_app', //remote
+  'secret': 'leonardodicapriojustwonanoscar'
 };
