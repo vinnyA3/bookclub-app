444d6a7fc7f7f093cb7d4718335c3d14c33985c3
vinnya3 <vinaceto@yahoo.com>
2016-02-29 11:55:46 -0500

    added authentication middleware

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,3 +1,28 @@
 var User = require('../models/user.js'),
     OutstandingRequest = require('../models/outstanding-requests.js'),
     SwapRequests = require('../models/swap-requests.js');
+
+module.exports = {
+  // =============== AUTHENTICATION MIDDLEWARE ===========
+  function ensureAuthenticated(req,res,next){
+    if(!req.headers.authorization){
+      return res.status(401).send({message: 'Please make sure that your request has an Authorization header!'});
+    }
+    var token = req.headers.authorization.split(' ')[1];
+
+    var payload = null;
+    try{
+      payload = jwt.decode(token, config.secret);
+    }
+    catch(err){
+      return res.status(401).send({message: err.message});
+    }
+    if (payload.exp <= moment().unix()){
+      return res.status(401).send({message: 'Token has expired!'});
+    }
+    req.user = payload.sub;
+    next();
+  };
+
+  
+};
