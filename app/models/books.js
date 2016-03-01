2daf3da6a385fc3939561c91bae2cebfcb75e710
vinnya3 <vinaceto@yahoo.com>
2016-02-29 22:04:28 -0500

    changed name

--- app/models/books.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var BookSchema = mongoose.Schema({
-  title: String,
-  imgLink: String,
-  owner: [{type: mongoose.SchemaTypes.ObjectId, ref: 'User'}]
-});
-
-mongoose.exports = mongoose.model('Book', BookSchema);
