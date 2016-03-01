9667d1d3beb0ff808084103a2ce30f3e00c4ff57
vinnya3 <vinaceto@yahoo.com>
2016-03-01 02:03:11 -0500

    added other user acc routes, added put route for current user account

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -34,7 +34,7 @@ module.exports = function(app,express){
   //================ API ROUTES ==========================
 
   // ===== ALL BOOKS =====
-  //books
+  //get books
   router.get('/all-books', ensureAuthenticated, function(req,res){
     //return all books
     Book.find({}, function(err,books){
@@ -67,7 +67,7 @@ module.exports = function(app,express){
        });
      });
   })
-  //get user's books - whole user
+  //get user and populate books
   .get(ensureAuthenticated, function(req,res){
     User.findOne({"_id": req.user})
         .populate('books')
@@ -78,33 +78,88 @@ module.exports = function(app,express){
           return res.send(user);
         });
   });
-  //router.get('/userbooks', function(req,res){ });
-  //post - create OutstandingRequest
-  router.post('/make-book-request', ensureAuthenticated, function(req,res){
-    //create the new book request
-    var newOutstandingRequest = new OutstandingRequest();
-    newOutstandingRequest.to = req.body.toUser,
-    newOutstandingRequest.requestedBook = req.body.requested,
-    newOutstandingRequest.swapFor = req.body.swapfor,
-    newOutstandingRequest.status = 'Not Yet Approved',
-    newOutstandingRequest.belongsTo = req.user;
 
-    //save the request
-    newOutstandingRequest.save(function(err,swapRequest){
-      if(err){
-        return res.send(err);
-      }
-      //update the Users's outstandingRequests array - push in swapRequest ObjectId
-      User.update({'_id': req.user}, {$push: {outstandingRequests: swapRequest.id}}, function(err,user){
+ //====== OTHER USERS' PROFILE PAGE ====
+  router.route('/user/:user_id')
+    //get the page info and selected book
+    .get(ensureAuthenticated, function(req,res){
+      //get the the clicked user's accounts page
+      User.find({'_id': req.params.user_id}, function(err,user){
         if(err){
           return res.send(err);
         }
-        //return success
-        return res.send({success:true, message: 'Outstanding request Added!', user: user});
+        if(!user){
+          return res.send({message: 'Failed to retrieve User Information.  Please try again later'});
+        }else{
+          return res.send(user);
+        }
       });
-    }); //end outstanding request save
-  });
+    })
+    .post(ensureAuthenticated, function(req,res){
+      //create the new book request
+      var newOutstandingRequest = new OutstandingRequest();
+      newOutstandingRequest.to = req.params.user_id,
+      newOutstandingRequest.requestedBook = req.body.requested,
+      newOutstandingRequest.swapFor = req.body.swapfor,
+      newOutstandingRequest.status = 'Not Yet Approved',
+      newOutstandingRequest.belongsTo = req.user;
 
+      //save the request
+      newOutstandingRequest.save(function(err,swapRequest){
+        if(err){
+          return res.send(err);
+        }
+        //update the Users's outstandingRequests array - push in swapRequest ObjectId
+        User.update({'_id': req.user}, {$push: {outstandingRequests: swapRequest.id}}, function(err,user){
+          if(err){
+            return res.send(err);
+          }
+          //return success
+          return res.send({success:true, message: 'Outstanding request Added!', user: user});
+        });
+      }); //end outstanding request save
+    });
+
+  //==== USER ACCOUNT ======
+  router.route('/account')
+    .get(ensureAuthenticated, function(req,res){
+        User.find({'_id': req.user}, function(err,user){
+           if(err){
+             return res.send(err);
+           }
+           //return the user
+           return res.send(user);
+        });
+    })
+    .put(ensureAuthenticated, function(req,res){
+      User.find({'_id':req.user}, function(err,user){
+        if(err){
+          return res.send(err);
+        }
+        //if we have options to change in the request, set the updated properties on the user
+        if(req.body.name){user.name = req.body.name;}
+        if(req.body.email){user.email = req.body.email;}
+        if(req.body.password){user.name = req.body.password;}
+
+        //save the user
+        user.save(function(err){
+            if(err){
+              return res.send(err);
+            }
+            //return a success message
+            return res.send({message: 'Information Updated!'});
+        });
+
+      });
+    })
+    .delete(ensureAuthenticated, function(req,res){
+      User.remove({'_id': req.user}, function(err){
+        if(err){
+          return res.send(err);
+        }
+        return res.send({message: 'successfully deleted!'});
+      });
+    });
 
   //return the express router
   return router;
