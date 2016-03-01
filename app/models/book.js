b316eda2648afd17f34cb439c19b0437247548cc
vinnya3 <vinaceto@yahoo.com>
2016-02-29 22:06:30 -0500

    changed file name

---
+++ app/models/book.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+
+var BookSchema = mongoose.Schema({
+  title: String,
+  imgLink: String,
+  owner: [{type: mongoose.SchemaTypes.ObjectId, ref: 'User'}]
+});
+
+module.exports = mongoose.model('Book', BookSchema);
