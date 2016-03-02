1278024f5d2fcf0d281c320d89fb3602b71360ff
vinnya3 <vinaceto@yahoo.com>
2016-03-01 20:27:55 -0500

    removed .js extensions from exports

--- app/routes/auth-routes.js
+++ app/routes/auth-routes.js
@@ -1,4 +1,4 @@
-var User = require('../models/user.js'),
+var User = require('../models/user'),
     config = require('../../config/config'),
     jwt = require('jwt-simple'),
     moment = require('moment');
