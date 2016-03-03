87237517d6a7b345bdbbeee3883f48b35424faa1
vinnya3 <vinaceto@yahoo.com>
2016-03-03 01:31:24 -0500

    added delete to user-books route

--- public/app/app-routes.js
+++ public/app/app-routes.js
@@ -6,28 +6,40 @@ angular.module('appRoutes',['ui.router'])
           views:{
             '':{templateUrl:'app/views/pages/home.html', controller: 'libraryController as library'},
             'nav@home': {templateUrl: 'app/views/partials/navbar.html'},
-          }
+          },
+          resolve: {
+				    	loginRequired: loginRequired
+			  	}
       })
       .state('account', {
           url:'/account',
           views:{
             '':{templateUrl:'app/views/pages/account.html'},
             'nav@account': {templateUrl: 'app/views/partials/navbar.html'}
-          }
+          },
+          resolve: {
+					    loginRequired: loginRequired
+				  }
       })
       .state('inbox', {
          url:'/inbox',
          views:{
            '':{templateUrl:'app/views/pages/inbox.html', controller: 'inboxController as inbox'},
            'nav@inbox':{templateUrl:'app/views/partials/navbar.html'}
-         }
+         },
+         resolve: {
+				   	loginRequired: loginRequired
+				 }
       })
       .state('your-books',{
          url:'/your-books',
          views:{
            '':{templateUrl:'app/views/pages/my-books.html', controller: 'yourBooksController as books'},
            'nav@your-books':{templateUrl:'app/views/partials/navbar.html'}
-         }
+         },
+         resolve: {
+				  	loginRequired: loginRequired
+				 }
       })
       .state('user',{
         url:'/user/:id?:book_id',
@@ -38,7 +50,10 @@ angular.module('appRoutes',['ui.router'])
           }
         },
           'nav@user':{templateUrl:'app/views/partials/navbar.html'}
-        }
+        },
+        resolve: {
+					loginRequired: loginRequired
+				}
       });
 
       //otherwise, redirect to home
@@ -46,5 +61,18 @@ angular.module('appRoutes',['ui.router'])
 
       //set base href # to /
       $locationProvider.html5Mode(true);
+
+
+      //========================  LOGIN REQUIRED RESOLVE FUNCTION  ===========================
+		function loginRequired($q,$location,$auth){
+			var deffered = $q.defer();
+			if($auth.isAuthenticated()){
+			   	deffered.resolve();
+			}else{
+				//redirect to home page
+				$location.path('/');
+			}
+			return deffered.promise;
+		}
 
   });
