bb3d09dd45ba3b4a017fcf85591e1f20c3591f32
vinnya3 <vinaceto@yahoo.com>
2016-03-03 15:07:15 -0500

    added getbooks, deletebooks function

--- public/app/controllers/myBooksController.js
+++ public/app/controllers/myBooksController.js
@@ -1,9 +1,51 @@
-angular.module('yourBooksCtrl', [])
-  .controller('yourBooksController', function(){
+angular.module('yourBooksCtrl', ['bookService'])
+  .controller('yourBooksController', function(Book){
     var vm = this;
-    vm.yourBooks = [
-      {src: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRQ9rKopiX1-yJi8XnIiDN-6D1QruyWvJlDc9Vu6sa_lZM2BjEM'},
-      {src: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcT_sMnSRutk9H2zx_7hpJwv658ZgUbaRUFJ_h7fYo93LydERdCx'},
-      {src: 'http://flavorwire.files.wordpress.com/2012/08/gatsby.jpg'}
-    ];
+
+    vm.yourBooks = [];
+
+    vm.getBooks = function(){
+      Book.getUserBooks()
+        .then(function(data){
+
+          vm.yourBooks = data.books;
+        })
+        .catch(function(data){
+          console.log('error fetching books');
+        });
+    };
+    //add book
+    vm.addBook = function(){
+      if(vm.book.title === ''){
+        return;
+      }
+      Book.addUserBook(vm.book.title)
+        .then(function(data){
+          vm.book.title = '';
+          //refresh the data
+          vm.getBooks();
+        })
+        .catch(function(data){
+          console.log('error adding book');
+        });
+    };
+
+    vm.deleteBook = function(id){
+      //get book id
+      var bookId = vm.yourBooks[id]._id;
+
+      Book.deleteUserBook(bookId)
+        .then(function(data){
+          console.log('book deleted');
+          //refresh the book list
+          vm.getBooks();
+        })
+        .catch(function(data){
+          console.log('failed to delete book');
+        });
+    }
+
+    //call get user books
+    vm.getBooks();
+
   });
