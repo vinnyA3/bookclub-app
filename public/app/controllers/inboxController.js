dedd68b729e620eede5a711535d23e2207fabb69
vinnya3 <vinaceto@yahoo.com>
2016-03-03 19:01:47 -0500

    added getRequests function

--- public/app/controllers/inboxController.js
+++ public/app/controllers/inboxController.js
@@ -1,27 +1,32 @@
-angular.module('inboxCtrl', [])
-  .controller('inboxController', function(){
+angular.module('inboxCtrl', ['inboxService'])
+  .controller('inboxController', function(Inbox){
 
     var vm = this;
-
+    vm.noRequests = false;
+    vm.noRequestMessage = 'There are no book swap requests.';
     vm.modalShown = false;
     vm.approvedInfo = {};
+    vm.swapRequests = [];
 
-    vm.swapRequests = [
-      {name: 'Julie', book: 'Cosmos'},
-      {name: 'Julie', book: 'Cosmos'},
-      {name: 'Julie', book: 'Cosmos'},
-      {name: 'Julie', book: 'Cosmos'},
-      {name: 'Julie', book: 'Cosmos'},
-      {name: 'Julie', book: 'Cosmos'},
-      {name: 'Julie', book: 'Cosmos'},
-      {name: 'Julie', book: 'Cosmos'},
-      {name: 'Julie', book: 'Cosmos'},
-    ];
+    vm.getRequests = function(){
+        Inbox.getRequests()
+          .then(function(data){
+              vm.swapRequests = data[0].bookRequests;
+              if(!vm.swapRequests.length){
+                vm.noRequests = true;
+              }
+          })
+          .catch(function(data){
+            console.log('error....');
+          });
+    };
 
     vm.toggleModal = function(name,book){
         vm.modalShown = !vm.modalShown;
         vm.approvedInfo.name = name;
         vm.approvedInfo.book = book;
     };
+
+    vm.getRequests();
 
   });
