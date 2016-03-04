bc84238527b6c8261ebc09377f44793b68d1b67a
vinnya3 <vinaceto@yahoo.com>
2016-03-03 19:03:36 -0500

    fixed url

--- public/app/services/inboxService.js
+++ public/app/services/inboxService.js
@@ -1,24 +1,24 @@
 angular.module('inboxService',[])
   .factory('Inbox', function($q,$http){
     var inboxService = {
-      getRequests: getRequests,
-      createRequest: createRequest
+      getRequests: getRequests
+    //  createRequest: createRequest
     };
 
-    function getRequests(id){
+    function getRequests(){
         var deffered = $q.defer();
 
-        $http.get('/inbox')
+        $http.get('/api/inbox')
           .success(function(data){
             deffered.resolve(data);
           })
           .error(function(data){
-            deffered.reject();
+            deffered.reject(data);
           });
         //return promise object
         return deffered.promise;
     };
 
     return inboxService;
-    
+
   });
