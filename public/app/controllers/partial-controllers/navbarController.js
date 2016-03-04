8db315fc0463c20d7cf91a3c235f30abe8e486b4
vinnya3 <vinaceto@yahoo.com>
2016-03-03 19:02:30 -0500

    added logout

--- public/app/controllers/partial-controllers/navbarController.js
+++ public/app/controllers/partial-controllers/navbarController.js
@@ -2,11 +2,6 @@ angular.module('navbarCtrl', ['satellizer'])
   .controller('navbarController', function($auth, $location){
     var vm = this;
 
-    /*isAuthenticated function
-    vm.isAuthenticated = function(){
-      return $auth.isAuthenticated();
-    };*/
-
     //logout function
     vm.logOut = function(){
       $auth.logout()
