d67859fa5d6df57f8192387abc24eb0651b68355
vinnya3 <vinaceto@yahoo.com>
2016-03-04 19:23:11 -0500

    modified search book to take in params, added setRequestedBook function

--- public/app/controllers/usersController.js
+++ public/app/controllers/usersController.js
@@ -1,19 +1,17 @@
 angular.module('usersCtrl', ['usersService'])
-  .controller('usersController', function(User,$stateParams){
+  .controller('usersController', function(User, $stateParams, $location, $anchorScroll){
+
     var vm = this;
+    var bookId = $stateParams.book_id;
 
     vm.userData = {};
     vm.requestedBook = {};
 
-    console.log($stateParams.book_id);
-    //search for requested book
-    function searchRequestedBook(books){
-      var bookId = $stateParams.book_id;
-
+    //search for requested book - id must be a book id
+    function searchRequestedBook(books, id){
       //refactor to use better algorithm
       for(var i = 0; i < books.length; ++i){
-        
-          if( books[i]._id === bookId ){
+          if( books[i]._id === id ){
             return books[i];
           }
       };
@@ -22,13 +20,22 @@ angular.module('usersCtrl', ['usersService'])
     vm.getUserInfo = function(){
       User.getUserInfo($stateParams.id)
         .then(function(data){
-
             vm.userData = data[0];
-            vm.requestedBook = searchRequestedBook(data[0].books);
+            vm.requestedBook = searchRequestedBook(vm.userData.books, bookId);
         })
         .catch(function(data){
             console.log('error...');
         });
+    };
+
+    vm.setRequestedBook = function(index){
+       var book_id = vm.userData.books[index]._id;
+       //search for the clicked book
+       vm.requestedBook = searchRequestedBook(vm.userData.books, book_id);
+
+       $location.hash('requested');
+
+       $anchorScroll();
     };
 
     //cal get user info
