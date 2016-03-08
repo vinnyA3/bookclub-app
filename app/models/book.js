a156dae57e20cbcf4e286449eb63b289bd20391e
vinnya3 <vinaceto@yahoo.com>
2016-03-07 20:17:38 -0500

    removed array brackets for owner reference field

--- app/models/book.js
+++ app/models/book.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var BookSchema = mongoose.Schema({
   title: String,
   imgLink: String,
-  owner: [{type: mongoose.SchemaTypes.ObjectId, ref: 'User'}]
+  owner: {type: mongoose.SchemaTypes.ObjectId, ref: 'User'}
 });
 
 module.exports = mongoose.model('Book', BookSchema);
