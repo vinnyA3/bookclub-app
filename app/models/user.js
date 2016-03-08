597b4fa28bcea2f43cc487b6bb129404cc7fcf6e
vinnya3 <vinaceto@yahoo.com>
2016-03-07 20:18:06 -0500

    removed comments

--- app/models/user.js
+++ app/models/user.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose'),
-    bcrypt = require('bcrypt-nodejs');
+    bcrypt = require('bcrypt-nodejs'),
+    BookRequest = require('./book-requests.js').schema;
 
 var UserSchema = mongoose.Schema({
     name: String,
@@ -9,13 +10,6 @@ var UserSchema = mongoose.Schema({
     books: [{type: mongoose.SchemaTypes.ObjectId, ref:'Book'}],
     bookRequests: [BookRequest]
 });
-
-/*User Middleware - remove all Book docs and book requests that reference the removed User
-UserSchema.pre('findOneAndUpdate', function(req,res,next){
-  //this.model('BookRequest').remove({belongsTo: this._id});
-  console.log(req.user);
-  this.model('Book').remove({owner: this._id}, next);
-});*/
 
 UserSchema.methods.hashPassword = function(password){
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8,null));
