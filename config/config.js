13f8228f0e408408b5b438b5cdecbc9791728764
vinnya3 <vinaceto@yahoo.com>
2016-03-15 01:33:31 -0400

    secret

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   //'db': 'mongodb://127.0.0.1:27017/bookclub_app', //local
-  'db': 'mongodb://ace:herbcity@ds011459.mlab.com:11459/bookclub_app', //remote
-  'secret': 'leonardodicapriojustwonanoscar'
+  'db': 'mongodb://<secret name>:<secret pass>@ds011459.mlab.com:11459/bookclub_app', //remote
+  'secret': '<secret>'
 };
